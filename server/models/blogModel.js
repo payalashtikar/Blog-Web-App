6f6d3c4273156b11393e344bfd43abb64d66b181
unknown <payalashtikar@gmail.com>
2024-04-01 23:12:26 +0530

    soft-delete-implement

--- server/models/blogModel.js
+++ server/models/blogModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const blogSchema = new mongoose.Schema({
     title: { type: String },
     description: { type: String },
+    isDeleted: { type: Boolean },
+    deletedAt: { type: Date },
     user: { type: mongoose.Types.ObjectId, ref: "User" },
 },
     { timestamps: true }
